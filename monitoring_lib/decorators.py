23a707356e663610c2c90f937bff7450832fbc35
Elena Nazarova <nazzarova.in@gmail.com>
2025-05-12 15:31:04 +0300

    Update decorators

--- monitoring_lib/decorators.py
+++ monitoring_lib/decorators.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from functools import wraps
+from zoneinfo import ZoneInfo,ZoneInfoNotFoundError
 from monitoring_lib.producer_kafka import kafka_logger
 import time
 
@@ -12,10 +14,22 @@ def get_client_ip(request):
 
 
 def log_to_kafka(view_func):
+  @wraps(view_func)
   def wrapper(self, request, *args, **kwargs):
     start = time.time()
     response = view_func(self, request, *args, **kwargs)
     end = time.time()
+
+    try:
+      request_data = dict(request.data)
+    except Exception:
+      request_data = {}
+
+    secret_keys = {'password', 'token', 'access_token', 'refresh_token'}
+    data = {}
+    for k,v in request_data.items():
+      if k not in secret_keys:
+        data[k]=v
 
     log_data = {
       "method": request.method,
@@ -23,10 +37,11 @@ def log_to_kafka(view_func):
       "status": response.status_code,
       "duration": round(end  - start, 3),
       "user_id": request.user.id if request.user.is_authenticated else None,
-      "timestamp":datetime.now().isoformat() + "Z",
+      "timestamp": datetime.now(ZoneInfo("UTC")).isoformat(),
       "ip_address": get_client_ip(request),
-    }
+      "request_data": data,
 
+    }
     kafka_logger.send_log(log_data)
     return response
   return wrapper
