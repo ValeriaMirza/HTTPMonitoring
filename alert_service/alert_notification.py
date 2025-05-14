bb6a6e6c53aa33380cee5eaf89389f416df1af8f
Valeria Mirza <valeriamirza5@gmail.com>
2025-05-14 15:21:18 +0300

    Modify redis logic

--- alert_service/alert_notification.py
+++ alert_service/alert_notification.py
@@ -1,13 +1,10 @@
 import os
 from dotenv import load_dotenv
-
 import json
 import time
 import requests
-
 from elasticsearch8 import Elasticsearch
 import redis
-
 
 load_dotenv()
 ELASTICSEARCH_URL = os.getenv("ELASTICSEARCH_URL")
@@ -15,67 +12,77 @@ ELASTICSEARCH_INDEX = os.getenv("ELASTICSEARCH_INDEX")
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
-
-
 elastic = Elasticsearch(hosts=[ELASTICSEARCH_URL])
-
 r = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
 
+LAST_SEEN_KEY = "last_seen_timestamp"
 
-def is_seen(log_id):
-  return r.sismember("seen_logs", log_id)
+def get_last_seen_time():
+    return r.get(LAST_SEEN_KEY)
 
-
-def mark_seen(log_id):
-  r.sadd("seen_logs", log_id)
+def update_last_seen_time(ts):
+    r.set(LAST_SEEN_KEY, ts)
 
 def create_index():
-  if not elastic.indices.exists(index=ELASTICSEARCH_INDEX):
-    print(f"Index {ELASTICSEARCH_INDEX} doesn't exist, creating a new one...")
-    elastic.indices.create(index=ELASTICSEARCH_INDEX)
-  else:
-    print(f"Index {ELASTICSEARCH_INDEX} exists! ")
+    if not elastic.indices.exists(index=ELASTICSEARCH_INDEX):
+        elastic.indices.create(index=ELASTICSEARCH_INDEX)
 
+def find_logs_problematic(last_ts):
+    must_conditions = []
 
-def find_logs_problematic():
-  query = {
-    "query": {
-      "bool": {
-        "should": [
-          {"terms": {"status": [400, 401, 403, 404, 500]}},
-          {"range": {"duration": {"gt": 2}}}
-        ],
-        "minimum_should_match": 1
-      }
+    if last_ts:
+        must_conditions.append({"range": {"timestamp": {"gt": last_ts}}})
+
+    query = {
+        "query": {
+            "bool": {
+                "must": must_conditions,
+                "should": [
+                    {"terms": {"status": [400, 401, 403, 404, 500]}},
+                    {"range": {"duration": {"gt": 2}}}
+                ],
+                "minimum_should_match": 1
+            }
+        },
+        "sort": [{"timestamp": "asc"}]
     }
-  }
 
-  result = elastic.search(index=ELASTICSEARCH_INDEX, body=query)
-  return result["hits"]["hits"]
+    result = elastic.search(index=ELASTICSEARCH_INDEX, body=query)
+    logs = result["hits"]["hits"]
+    return logs
 
 def send_alert_tg(text):
-  url =  f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
-  payload = {
-    "chat_id": TELEGRAM_CHAT_ID,
-    "text": text,
-    "parse_mode": "Markdown"
-  }
-  requests.post(url, json=payload)
-
+    url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
+    requests.post(url, json=payload)
 
 def main():
     print("Alert Service started....")
     create_index()
 
     while True:
-        problematic_logs = find_logs_problematic()
+        last_seen = get_last_seen_time()
+        problematic_logs = find_logs_problematic(last_seen)
+
+        latest_ts = last_seen
 
         for log in problematic_logs:
-            log_id = log["_id"]
-            if not is_seen(log_id):
-                message = f" *Problematic log:* ```\n{json.dumps(log['_source'], indent=2)}```"
-                send_alert_tg(message)
-                mark_seen(log_id)
+            log_ts = log['_source']['timestamp']
+            if last_seen and log_ts <= last_seen:
+                continue
+
+            message = f" *Problematic log:* ```\n{json.dumps(log['_source'], indent=2)}```"
+            send_alert_tg(message)
+
+            if not latest_ts or log_ts > latest_ts:
+                latest_ts = log_ts
+
+        if latest_ts:
+            update_last_seen_time(latest_ts)
 
         time.sleep(10)
 
